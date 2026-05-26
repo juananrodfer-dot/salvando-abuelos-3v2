f0a451186c54253ab85bf837dfc14c139088abe0
juananrodfer-dot <juananrodfer@gmail.com>
2026-05-26 11:00:10 +0200

    final con monedas

--- main.ts
+++ main.ts
@@ -10,12 +10,24 @@ namespace SpriteKind {
     export const FantasmaBlanco = SpriteKind.create()
     export const FantasmaCirculo = SpriteKind.create()
     export const ProjectileIA = SpriteKind.create()
+    export const moneda = SpriteKind.create()
+    export const m2 = SpriteKind.create()
+    export const m3 = SpriteKind.create()
+    export const m4 = SpriteKind.create()
+    export const m5 = SpriteKind.create()
+    export const m6 = SpriteKind.create()
+    export const m7 = SpriteKind.create()
+    export const m8 = SpriteKind.create()
+    export const m9 = SpriteKind.create()
 }
 function CargaNivel (n: number) {
     NivelActual = 2
     music.stopAllSounds()
     music.setVolume(20)
     sprites.destroy(niño_malo)
+    sprites.destroy(m1)
+    sprites.destroy(m2)
+    sprites.destroy(m3)
     tiles.setCurrentTilemap(tilemap`nivel2`)
     tiles.placeOnRandomTile(jugador, sprites.dungeon.doorClosedNorth)
     music.play(music.createSong(hex`0078000408020400001c00010a006400f4016400000400000000000000000000000000050000043c0004000800010c08000c00011110001400010a14001800010f1c002000010c24002800010a28002c0001122c003000010f34003800010d38003c00011204001c00100500640000041e000004000000000000000000000000000a040004120008001000011818002000011430003800011405001c000f0a006400f4010a00000400000000000000000000000000000000021e0008000c00010a1400180001111c002000010a28002c00011134003800010c06001c00010a006400f401640000040000000000000000000000000000000002250008000c00010812001300011817001800010820002400021216250026000116320033000106`), music.PlaybackMode.LoopingInBackground)
@@ -105,7 +117,46 @@ function CargaNivel (n: number) {
     fantasmita.follow(jugador, 30)
     fantasma.follow(jugador, 30)
     fantasmon.follow(jugador, 30)
+    m4 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m4)
+    m5 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m5)
+    m6 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m6)
+    tiles.placeOnTile(m4, tiles.getTileLocation(1, 1))
+    tiles.placeOnTile(m5, tiles.getTileLocation(3, 12))
+    tiles.placeOnTile(m6, tiles.getTileLocation(10, 4))
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m3, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m3)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, jugador)
 })
@@ -158,6 +209,39 @@ function Nivel0 (núm: number) {
     info.setLife(15)
     game.showLongText("encuentra el baston magico para pasar de nivel, cuidado con el laser", DialogLayout.Full)
     music.setVolume(20)
+    m1 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.moneda)
+    m2 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m2)
+    m3 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m3)
+    tiles.placeOnTile(m1, tiles.getTileLocation(0, 6))
+    tiles.placeOnTile(m2, tiles.getTileLocation(7, 1))
+    tiles.placeOnTile(m3, tiles.getTileLocation(14, 0))
     music.play(music.createSong(hex`0078000408030a00001c00010a006400f4016400000400000000000000000000000000050000048800000004000125040008000306142708000c0001220c001000030d242510001400021e2218001c0002111e1c0020000208272400280002122228002c0001242c003000010a30003400012238003c0003060c1e40004400022027440048000308121d48004c00012550005400030c22255400580003141b1e58005c0004050c22255c006000030d161d01001c000f05001202c102c20100040500280000006400280003140006020004060020002100010602001c000c960064006d019001000478002c010000640032000000000a060005060020002400010803001c0001dc00690000045e010004000000000000000000000564000104000325000c00100002061124002800010d30003400010634003800010f4000440001064c005000010f04001c00100500640000041e000004000000000000000000000000000a0400044a0004000800010c08000c00010d14001800010818001c00010d20002400010524002800010a3000340001123400380001083c004000020a0d48004c00010d5000540002081158005c00010d05001c000f0a006400f4010a00000400000000000000000000000000000000027e0000000400011b04000800011e08000c0001220c001000011b10001400011e14001800012220002400012224002800011b28002c00011e2c003000012230003400011934003800011d38003c00012240004400011d44004800012248004c0001194c005000011d50005400012254005800011958005c00011d5c006000012206001c00010a006400f401640000040000000000000000000000000000000002360008000c00011e18001c00012528002c00011930003400012738003c00011948004c00011e4c00500001195400580001225c006000011e07001c00020a006400f40164000004000000000000000000000000000000000306004b004c00010c08001c000e050046006603320000040a002d00000064001400013200020100021e0018001c00012720002400011b38003c0001254c00500001225c006000012509010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c80006004c004d000112`), music.PlaybackMode.LoopingInBackground)
 }
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
@@ -184,6 +268,18 @@ sprites.onOverlap(SpriteKind.FantasmaCirculo, SpriteKind.Player, function (sprit
             tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
         }
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m6, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m6)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m9, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m9)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
 })
 sprites.onOverlap(SpriteKind.FantasmaBlanco, SpriteKind.Player, function (sprite, otherSprite) {
     if (Ataque == 1) {
@@ -291,6 +387,9 @@ function nivel3 (núm: number) {
     sprites.destroy(fantasma)
     sprites.destroy(fantasmon)
     sprites.destroy(fantasmita)
+    sprites.destroy(m4)
+    sprites.destroy(m5)
+    sprites.destroy(m6)
     fantasma_azul = sprites.create(img`
         ........................
         ........................
@@ -406,6 +505,39 @@ function nivel3 (núm: number) {
     tiles.setCurrentTilemap(tilemap`nivel0`)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
     game.showLongText("Encuentra la radio de nuestros abuelos para escuchar musica con ellos", DialogLayout.Center)
+    m7 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m7)
+    m8 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m8)
+    m9 = sprites.create(img`
+        . . b b b b . . 
+        . b 5 5 5 5 b . 
+        b 5 d 3 3 d 5 b 
+        b 5 3 5 5 1 5 b 
+        c 5 3 5 5 1 d c 
+        c d d 1 1 d d c 
+        . f d d d d f . 
+        . . f f f f . . 
+        `, SpriteKind.m9)
+    tiles.placeOnTile(m7, tiles.getTileLocation(26, 8))
+    tiles.placeOnTile(m8, tiles.getTileLocation(42, 2))
+    tiles.placeOnTile(m9, tiles.getTileLocation(74, 3))
     controller.moveSprite(jugador, 100, 0)
     jugador.ay = 400
 }
@@ -518,6 +650,12 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     100,
     true
     )
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m4, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m4)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Izquierda = 1
@@ -840,6 +978,9 @@ function CargaNivel4 (núm: number) {
     sprites.destroy(fantasmon)
     sprites.destroy(fantasma)
     sprites.destroy(fantasmita)
+    sprites.destroy(m7)
+    sprites.destroy(m8)
+    sprites.destroy(m9)
     tiles.setCurrentTilemap(tilemap`nivel7`)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
     IA = sprites.create(img`
@@ -896,8 +1037,6 @@ function CargaNivel4 (núm: number) {
     BarraIA = statusbars.create(60, 8, StatusBarKind.Health)
     BarraIA.max = 5
     BarraIA.value = vidasIA
-    puntos = 0
-    info.setScore(puntos)
     BarraIA.setPosition(122, 6)
     IA.ay = 0
     IA.vy = 30
@@ -963,6 +1102,12 @@ function CargaNivel4 (núm: number) {
     abuelo_1.setPosition(590, 40)
     IA.setPosition(700, 75)
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m5, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m5)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral4, function (sprite, location) {
     info.changeLifeBy(-1)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
@@ -995,6 +1140,12 @@ sprites.onOverlap(SpriteKind.Fantasma, SpriteKind.Player, function (sprite, othe
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     info.changeLifeBy(-1)
     tiles.placeOnRandomTile(jugador, assets.tile`miMosaico2`)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m8, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m8)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -1072,6 +1223,12 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.moneda, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m1)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`EXIT`, function (sprite, location) {
     if (tienebaston == 1) {
         CargaNivel(1)
@@ -1083,6 +1240,18 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`EXIT`, function (sprite, loca
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, jugador)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m7, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m7)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.m2, function (sprite, otherSprite) {
+    info.changeScoreBy(10)
+    sprites.destroy(m2)
+    music.setVolume(50)
+    music.play(music.createSoundEffect(WaveShape.Square, 1, 2383, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
 })
 controller.B.onEvent(ControllerButtonEvent.Released, function () {
     Ataque = 0
@@ -1282,24 +1451,32 @@ let projectile: Sprite = null
 let projectile2: Sprite = null
 let mensajeabuelo = 0
 let abuelo_1: Sprite = null
-let puntos = 0
 let BarraIA: StatusBarSprite = null
 let vidasIA = 0
 let IA: Sprite = null
 let IADerrotada = 0
 let Izquierda = 0
 let tieneradio = 0
+let m8: Sprite = null
+let m7: Sprite = null
 let Tienelibro = 0
+let m9: Sprite = null
 let fantasma_azul: Sprite = null
 let Ataque = 0
 let tienebaston = 0
 let puededisparar = 0
 let mensajeLaser = 0
 let nivel = 0
+let m6: Sprite = null
+let m5: Sprite = null
+let m4: Sprite = null
 let fantasmita: Sprite = null
 let fantasmon: Sprite = null
 let fantasma: Sprite = null
 let jugador: Sprite = null
+let m3: Sprite = null
+let m2: Sprite = null
+let m1: Sprite = null
 let niño_malo: Sprite = null
 let NivelActual = 0
 scene.setBackgroundImage(assets.image`portada`)
